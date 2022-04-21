c9346ebb8cdde37fc0d6715e9dc5b1dd96abcfcb
EstebanLes <estebanles22@gamail.com>
2022-04-21 09:01:25 -0300

    agrego viewBinding

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -3,23 +3,25 @@ package com.example.recyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerview.adapter.SuperHeroAdapter
+import com.example.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+        private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initRecyclerView()
     }
 
     private fun initRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerSuperHero)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList)
+
+        binding.recyclerSuperHero.layoutManager = LinearLayoutManager(this)
+        binding.recyclerSuperHero.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList)
     }
 
 }
--- app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
@@ -1,25 +1,21 @@
 package com.example.recyclerview.adapter
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.recyclerview.R
 import com.example.recyclerview.SuperHero
+import com.example.recyclerview.databinding.ItemSuperheroBinding
 
 class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    private val superHero: TextView by lazy { view.findViewById<TextView>(R.id.tvSuperHeroName) }
-    private val realName: TextView by lazy { view.findViewById<TextView>(R.id.tvRealName) }
-    private val publisher: TextView by lazy { view.findViewById<TextView>(R.id.tvPublisher) }
-    private val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
+    val binding = ItemSuperheroBinding.bind(view)
+
 
     fun render(superHeroModel: SuperHero) {
-        superHero.text = superHeroModel.superHero
-        realName.text = superHeroModel.realName
-        publisher.text = superHeroModel.publisher
-        Glide.with(photo.context).load(superHeroModel.photo).into(photo)
+        binding.tvSuperHeroName.text = superHeroModel.superHero
+        binding.tvRealName.text = superHeroModel.realName
+        binding.tvPublisher.text = superHeroModel.publisher
+        Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
 
     }
 }
