f51540a5b22e3ba069bbd80a6486082ad6d765d1
EstebanLes <estebanles22@gamail.com>
2022-04-21 09:47:06 -0300

    agrego cardview

--- app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.recyclerview.adapter
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recyclerview.SuperHero
@@ -16,6 +17,11 @@ class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         binding.tvRealName.text = superHeroModel.realName
         binding.tvPublisher.text = superHeroModel.publisher
         Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
-
+        binding.ivSuperHero.setOnClickListener {
+            Toast.makeText(binding.ivSuperHero.context, "Clicked on ${superHeroModel.superHero}", Toast.LENGTH_SHORT).show()
+        }
+        itemView.setOnClickListener {
+            Toast.makeText(itemView.context, "Clicked on ${superHeroModel.superHero}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
